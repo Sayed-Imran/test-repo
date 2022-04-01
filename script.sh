8cd5f06e965ccb32eb21683cf8fa8287e4144aab
Sayed Imran <sayedimran00786@gmail.com>
2022-04-01 20:50:36 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,5 +2,5 @@ date
 echo "changes made"
 echo "It's working"
 sleep 10
-docker run -dit --name runningFromJenkins centos:7
-docker run -dit --name hello ubuntu
+docker run -dit  centos:7
+docker run -dit ubuntu
